c20533af5e896b29547c5ce51eb33d290e834a1f
Mohan Chanchal <chanchal.mohan@lisec.com>
2023-08-18 16:33:29 +0400

    Migration

--- ParkingManagement/Program.cs
+++ ParkingManagement/Program.cs
@@ -1,38 +1,48 @@
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.EntityFrameworkCore;
-
+using AutoMapper;
 using ParkingManagement;
+using ParkingManagement.Domain.Models;
 using ParkingManagement.Domain.Repositories.v1;
 using ParkingManagement.Domain.Services.v1;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var optionsBuilder = new DbContextOptionsBuilder<ParkingManagementDBContext>();
-optionsBuilder.UseSqlServer("");
-
-builder.Services.AddDbContext<ParkingManagementDBContext>(provider => new ParkingManagementDBContext(optionsBuilder.Options));
-builder.Services.AddScoped<UsersService>();
-builder.Services.AddScoped<UsersRepository>();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var optionsBuilder = new DbContextOptionsBuilder<ParkingManagementDBContext>();
+
+        string connection = "dev-shop-01-sql.database.windows.net; Database = TestCm; User Id = LisecAdmin; Password = evenly-2PNPSKFX7;";
+        optionsBuilder.UseSqlServer(connection);
+        builder.Services.AddDbContext<ParkingManagementDBContext>(provider => new ParkingManagementDBContext(optionsBuilder.Options));
+        builder.Services.AddScoped<UsersService>();
+        builder.Services.AddScoped<UsersRepository>();
+        builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();

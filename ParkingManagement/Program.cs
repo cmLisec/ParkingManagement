74857f46dbd0906b33868e45968585b8cab7d4dd
Mohan Chanchal <chanchal.mohan@lisec.com>
2023-08-21 19:10:26 +0400

    start up added

--- ParkingManagement/Program.cs
+++ ParkingManagement/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using ParkingManagement;
 using ParkingManagement.Domain.Repositories.v1;
@@ -5,50 +6,15 @@ using ParkingManagement.Domain.Services.v1;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        var app = builder.Build();
-
-        var optionsBuilder = new DbContextOptionsBuilder<ParkingManagementDBContext>();
-
-        string connection = "Server=tcp:parknow.database.windows.net,1433;Initial Catalog=ParkNow;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";";
-        optionsBuilder.UseSqlServer(connection);
-        builder.Services.AddDbContext<ParkingManagementDBContext>(provider => new ParkingManagementDBContext(optionsBuilder.Options));
-        builder.Services.AddScoped<UsersService>();
-        builder.Services.AddScoped<UsersRepository>();
-        builder.Services.AddScoped<ParkingCardService>();
-        builder.Services.AddScoped<ParkingCardRepository>();
-        builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-            {
-                Title = "Parking Management API",
-                Version = "v1"
-            });
-        });
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
---
+++ ParkingManagement/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using ParkingManagement.Domain.Repositories.v1;
+using ParkingManagement.Domain.Services.v1;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+
+namespace ParkingManagement
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        private const string _openApiDescription = @"A delivery tracking open api service";
+        /// <summary>
+        /// Constructor of Startup.
+        /// </summary>
+        /// <param name="env">Specify IWebHostEnvironment.</param>
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ParkingManagementDBContext>();
+
+            string connection = "Server=tcp:parknow.database.windows.net,1433;Initial Catalog=ParkNow;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";";
+            optionsBuilder.UseSqlServer(connection);
+            services.AddDbContext<ParkingManagementDBContext>(provider => new ParkingManagementDBContext(optionsBuilder.Options));
+            services.AddScoped<UsersService>();
+            services.AddScoped<UsersRepository>();
+            services.AddScoped<ParkingCardService>();
+            services.AddScoped<ParkingCardRepository>();
+            services.AddAutoMapper(typeof(MappingProfile));
+            services.AddControllers();
+            services.AddAutoMapper(typeof(MappingProfile));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Parking Management API", Version = "v1" });
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Parking Management API V1");
+            });
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                var controllerBuilder = endpoints.MapControllers();
+                if (!env.IsDevelopment())
+                    controllerBuilder.RequireAuthorization();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync(@"Hello World!");
+                });
+            });
+        }
+    }
+}

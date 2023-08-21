774114c2ad8aa1c011e1fbe4e4ccb28842c89b58
Mohan Chanchal <chanchal.mohan@lisec.com>
2023-08-21 18:40:24 +0400

    swagger prod build issue

--- ParkingManagement/Program.cs
+++ ParkingManagement/Program.cs
@@ -12,9 +12,7 @@ internal class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
         var optionsBuilder = new DbContextOptionsBuilder<ParkingManagementDBContext>();
 
@@ -27,7 +25,7 @@ internal class Program
         builder.Services.AddScoped<ParkingCardRepository>();
         builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-        var app = builder.Build();
+
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())

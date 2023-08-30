e65ea479853b8742f85bd0966a4cc0deeaf145ab
Mohan Chanchal <chanchal.mohan@lisec.com>
2023-08-30 12:07:28 +0400

    allow auth

--- ParkingManagement/Controllers/ParkingCardController.cs
+++ ParkingManagement/Controllers/ParkingCardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ParkingManagement.Controllers.OutputObject;
 using ParkingManagement.Domain.DTO;
 using ParkingManagement.Domain.Services.v1;
@@ -7,7 +8,7 @@ namespace ParkingManagement.Controllers
 {
     [Route("v1/api/[Controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class ParkingCardController : BaseController
     {
         private readonly ParkingCardService _service;
